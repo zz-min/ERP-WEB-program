59d24c9d632cf5d8d9b194eec0e14470b9ab3b5b
jimin <kjm77878@gmail.com>
2020-10-09 10:44:02 +0900

    arrayIssue Resolved
    50%processed

--- ex.js
+++ ex.js
@@ -1,36 +1,41 @@
-alert("JS정상동작중");
-var categoryAry=new Array();
-var num=4;
-    categoryAry[0].value="기본";
-    categoryAry[num-1].value="전체보기";
-
-    function zz(){
-        document.getElementById("ex").style.color="red";
-        // 버튼 객체 생성
-        var obj = document.createElement("<input type='button' value='동적버튼' />");
-        // dv영역에 해당 객체 추가
-        ex.appendChild(obj);    
+/*alert("JS정상동작중");*/
+const catArray= ["전체보기", "기본", "두번째내용", "세번째내용"];
+var num=4;/*나중에는 배열크기로 num초기화 */
+    catArray[0]="전체보기";/*맨마지막버튼 */
+    catArray[1]="기본";/*맨첫번째버튼 */
+    catArray[2]="두번째내용";
+    catArray[3]="세번째내용";
+    function btn_gen() {// 버튼 객체 생성
+        //alert("bbb"+catArray[0].toString()+"배열끝");
+        alert("배열크기=>"+catArray.length);
+        // for(var i=0; i<catArray.length; i++){
+        //     $("#kiki").append("<input type='button' value='동적버튼생성"+catArray[i].value+"'/>");
+        // }    
     }
-    /*
-    function Create(){
-        // 버튼 객체 생성
-        var obj = document.createElement("<input type='button' value='확인"+cnt+" />");
-        // 변수 증가
-        cnt++;
-        // dv영역에 해당 객체 추가
-        dv.appendChild(obj);    }*/
-    
-    function plus_Bt_Click(){
-        /* prompt(문자열, 초기값) */
-        var input_value = prompt("추가할 카테고리 명을 입력해주세요.", "카테고리 명");
-        /* if(name_value == true) else false */
-        if(input_value) alert("추가된 카테고리는 "+input_value+"입니다.");
-        else alert("카테고리 추가가 취소되었습니다.");
 
-    }
-    function arrangeBtn(array){
+/*$(document).ready(function (){// 버튼 객체 생성
+    for(var i=0; i<4; i++){//console.log(catArray[i]);
+        var str = catArray[i];
+        // alert();
+        var str_gen = "<input type='button' value='동적버튼생성" + console.log(str) + "'/>";
+        $("#kiki").append(str_gen); 
+    }   
+});*/
 
-    }
+/*$(document).ready(function (){// 버튼 객체 생성
+    alert("bbb");
+
+    for(var i=0; i<num; i++){
+        $("#kiki").append("<input type='button' value='동적버튼생성"+catArray[i].value+"'/>");
+    }    
+
+});*/
+
+/*window.onload(function catBtnCreate(){// 버튼 객체 생성
+    for(var i=0; i<catArray.length; i++){
+        $("#kiki").append("<input type='button' value='동적버튼생성_"+catArray[i].value+"'/>");
+    }   
+});*/
 /*
 function createButton(context, func) {
     var button = document.createElement("input");
@@ -47,3 +52,12 @@ window.onload = function() {
         // createButton(this.parentNode, this.onclick);
     });
 };*/
+
+function plus_Bt_Click(){
+    /* prompt(문자열, 초기값) */
+    var input_value = prompt("추가할 카테고리 명을 입력해주세요.", "카테고리 명");
+    /* if(name_value == true) else false */
+    if(input_value) alert("추가된 카테고리는 "+input_value+"입니다.");
+    else alert("카테고리 추가가 취소되었습니다.");
+
+}
