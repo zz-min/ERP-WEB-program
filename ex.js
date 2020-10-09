5a8f7f43179d7b5793df6df8818706334f35d646
jimin <kjm77878@gmail.com>
2020-10-09 12:49:33 +0900

    subPage-1 initial setup

--- ex.js
+++ ex.js
@@ -5,60 +5,38 @@ var num=4;/*나중에는 배열크기로 num초기화 */
     catArray[1]="기본";/*맨첫번째버튼 */
     catArray[2]="두번째내용";
     catArray[3]="세번째내용";
-    function btn_gen() {// 버튼 객체 생성
-        //alert("bbb"+catArray[0].toString()+"배열끝");
-        alert("배열크기=>"+catArray.length);
-        
-        // for(var i=0; i<catArray.length; i++){
-        //     $("#kiki").append("<input type='button' value='동적버튼생성"+catArray[i].value+"'/>");
-        // }    
+    function btnStyle(str){
+        str.style.backgroundColor="#008CBA";
+        str.style.color="white";
+        str.style.width="450px";
+        str.style.padding="50px";
+        str.style.margin="30px";
+        str.style.fontSize="80px";
     }
+    function btnCreate() {// 버튼 객체 생성
+        //alert("bbb"+catArray[0].toString()+"배열끝");
+        //alert("배열크기=>"+catArray.length);
+        $(".btnSpace").empty();
+        for(var i=1; i<catArray.length; i++){
+            var j=i+1;
+             $(".btnSpace").append("<input type='button' value='"+catArray[i].toString()+"' id='btn"+j+"'/>");
+             
+            var str = document.getElementById("btn"+j);
+            btnStyle(str);
+         }    
 
-/*$(document).ready(function (){// 버튼 객체 생성
-    for(var i=0; i<4; i++){//console.log(catArray[i]);
-        var str = catArray[i];
-        // alert();
-        var str_gen = "<input type='button' value='동적버튼생성" + console.log(str) + "'/>";
-        $("#kiki").append(str_gen); 
-    }   
-});*/
-
-/*$(document).ready(function (){// 버튼 객체 생성
-    alert("bbb");
-
-    for(var i=0; i<num; i++){
-        $("#kiki").append("<input type='button' value='동적버튼생성"+catArray[i].value+"'/>");
-    }    
-
-});*/
-
-/*window.onload(function catBtnCreate(){// 버튼 객체 생성
-    for(var i=0; i<catArray.length; i++){
-        $("#kiki").append("<input type='button' value='동적버튼생성_"+catArray[i].value+"'/>");
-    }   
-});*/
-/*
-function createButton(context, func) {
-    var button = document.createElement("input");
-    button.type = "button";
-    button.value = "im a button";
-    button.onclick = func;
-    context.appendChild(button);
-}
-
-window.onload = function() {
-    createButton(document.body, function() {
-        highlight(this.parentNode.childNodes[1]);
-        // Example of different context, copied function etc
-        // createButton(this.parentNode, this.onclick);
-    });
-};*/
+         $(".btnSpace").append("<input type='button' value='"+catArray[0].toString()+"' id='btn1'/>");         
+         btnStyle(btn1);
+    }
 
 function plus_Bt_Click(){
     /* prompt(문자열, 초기값) */
     var input_value = prompt("추가할 카테고리 명을 입력해주세요.", "카테고리 명");
-    /* if(name_value == true) else false */
+    /* if(name_value == true) else false 
     if(input_value) alert("추가된 카테고리는 "+input_value+"입니다.");
-    else alert("카테고리 추가가 취소되었습니다.");
+    else alert("카테고리 추가가 취소되었습니다.");*/
 
+    var num=catArray.length;
+    catArray[num]=input_value;
+    btnCreate();
 }
