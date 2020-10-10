113dad6633c5207133a71bbcd3941b4ec1532fca
jimin <kjm77878@gmail.com>
2020-10-11 00:16:06 +0900

    onclick함수만들다가 중단

--- ex.js
+++ ex.js
@@ -5,6 +5,7 @@ var num=4;/*나중에는 배열크기로 num초기화 */
     catArray[1]="기본";/*맨첫번째버튼 */
     catArray[2]="두번째내용";
     catArray[3]="세번째내용";
+
     function btnStyle(str){
         str.style.backgroundColor="#008CBA";
         str.style.color="white";
@@ -13,30 +14,37 @@ var num=4;/*나중에는 배열크기로 num초기화 */
         str.style.margin="30px";
         str.style.fontSize="80px";
     }
+
     function btnCreate() {// 버튼 객체 생성
         //alert("bbb"+catArray[0].toString()+"배열끝");
         //alert("배열크기=>"+catArray.length);
-        $(".btnSpace").empty();
+        $(".btnNew").empty();
         for(var i=1; i<catArray.length; i++){
             var j=i+1;
-             $(".btnSpace").append("<input type='button' value='"+catArray[i].toString()+"' id='btn"+j+"'/>");
+             $(".btnNew").append("<input type='button' value='"+catArray[i].toString()+"' id='btn"+j+"' onclick='button_click();'/>");
              
             var str = document.getElementById("btn"+j);
             btnStyle(str);
          }    
 
-         $(".btnSpace").append("<input type='button' value='"+catArray[0].toString()+"' id='btn1'/>");         
+         $(".btnNew").append("<input type='button' value='"+catArray[0].toString()+"' id='btn1' onclick='button_click();'/>");         
          btnStyle(btn1);
     }
+    
+        function button_click(){
+            
+        }
+   
 
-function plus_Bt_Click(){
-    /* prompt(문자열, 초기값) */
-    var input_value = prompt("추가할 카테고리 명을 입력해주세요.", "카테고리 명");
-    /* if(name_value == true) else false 
-    if(input_value) alert("추가된 카테고리는 "+input_value+"입니다.");
-    else alert("카테고리 추가가 취소되었습니다.");*/
+    
+    function plus_Bt_Click(){
+        /* prompt(문자열, 초기값) */
+        var input_value = prompt("추가할 카테고리 명을 입력해주세요.", "카테고리 명");
+        /* if(name_value == true) else false 
+        if(input_value) alert("추가된 카테고리는 "+input_value+"입니다.");
+        else alert("카테고리 추가가 취소되었습니다.");*/
 
-    var num=catArray.length;
-    catArray[num]=input_value;
-    btnCreate();
-}
+        var num=catArray.length;
+        catArray[num]=input_value;
+        btnCreate();
+    }
